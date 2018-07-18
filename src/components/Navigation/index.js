7e5dffa5d6cb03a8d9832711194108df8e5679c7
Katie Wang <ktwang16@mac.com>
2018-07-18 14:21:52 -0400

    Fixed auth navbar alignment

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -16,11 +16,16 @@ const Navigation = () =>
   </AuthUserContext.Consumer>
 
 const NavigationAuth = () =>
-<nav className="navbar navbar-inverse">
+<nav className="navbar navbar-default">
   <div className="container-fluid">
-    <div className="navbar-collapse collapse">
-      <ul className="nav navbar-nav">
-        <li className="landing"><Link to={routes.LANDING}><img className="logoImg img-responsive" src="assets/images/logo.png" alt=""/><p className="appTitle">Journal Buddy</p></Link></li>
+    <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul className="nav navbar-nav navbar-left">
+        <div className="navbar-header">
+        <li className="navbar-item"><Link to={routes.LANDING}><img className="logoImg img-responsive" src="assets/images/logo.png" alt=""/></Link></li>
+        <li className="navbar-item appName"><Link to={routes.LANDING}>JOURNAL BUDDY</Link></li>
+        </div>
+      </ul>
+      <ul className="nav navbar-nav navbar-right">
         <li className="navbar-item"><Link to={routes.HOME}>Home</Link></li>
         <li className="navbar-item"><Link to={routes.ACCOUNT}>Account</Link></li>
         <li className="btn-Nav"><SignOutButton /></li>
@@ -32,20 +37,12 @@ const NavigationAuth = () =>
 const NavigationNonAuth = () =>
 <nav className="navbar navbar-default">
   <div className="container-fluid">
-    {/* <!-- Brand and toggle get grouped for better mobile display --> */}
-    {/* <div className="navbar-header navbar-left">
-      <a className="navbar-brand" href="#">BUDDY</a>
-      <img alt="Brand" src="assets/images/logo.png"/>
-    </div> */}
-
-    {/* <!-- Collect the nav links, forms, and other content for toggling --> */}
     <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
       <ul className="nav navbar-nav navbar-left">
         <div className="navbar-header">
         <li className="navbar-item"><Link to={routes.LANDING}><img className="logoImg img-responsive" src="assets/images/logo.png" alt=""/></Link></li>
-        <li className="navbar-item appName"><Link to={routes.LANDING}>JOUUUUURNAL BUDDY</Link></li>
+        <li className="navbar-item appName"><Link to={routes.LANDING}>JOURNAL BUDDY</Link></li>
         </div>
-        
       </ul>
       <ul className="nav navbar-nav navbar-right">
         <li className="navbar-item"><Link to={routes.SIGN_IN}>LOGGGIN</Link></li>
@@ -54,24 +51,38 @@ const NavigationNonAuth = () =>
   </div>
 </nav>
 
-
-{/* <nav className="navbar navbar-default navbar-fixed-top">
-  <div className="container-fluid">
-    <div className="navbar-header">
-     <div className="navbarLeft">
-        <div className="navbar-item"><Link to={routes.LANDING}><img className="logoImg img-responsive" src="assets/images/logo.png" alt=""/><p className="appTitle"> Journal Buddy </p></Link></div>
-      </div>
-      <div className="navbarRight">
-        <div className="navbar-item"><Link to={routes.SIGN_IN}>SIGN IN</Link></div>
-      </div>
-    </div>  
-  </div>
-</nav> */}
-
 export default Navigation;
 
-      {/* <ul className="navbarList">
-        <li className="navbar-item"><Link to={routes.LANDING}><img className="logoImg img-responsive" src="assets/images/logo.png" alt=""/><p className="appTitle"> Journal Buddy </p></Link></li>
-        <li className="navbar-item"><Link to={routes.SIGN_IN}>SIGN IN</Link></li>
-        <button onClick={routes.SIGN_IN} className="btn-Nav">Log In</button>
-      </ul> */}
+      // {/* <ul className="navbarList">
+      //   <li className="navbar-item"><Link to={routes.LANDING}><img className="logoImg img-responsive" src="assets/images/logo.png" alt=""/><p className="appTitle"> Journal Buddy </p></Link></li>
+      //   <li className="navbar-item"><Link to={routes.SIGN_IN}>SIGN IN</Link></li>
+      //   <button onClick={routes.SIGN_IN} className="btn-Nav">Log In</button>
+      // </ul> */}
+
+
+// {/* <nav className="navbar navbar-default navbar-fixed-top">
+//   <div className="container-fluid">
+//     <div className="navbar-header">
+//      <div className="navbarLeft">
+//         <div className="navbar-item"><Link to={routes.LANDING}><img className="logoImg img-responsive" src="assets/images/logo.png" alt=""/><p className="appTitle"> Journal Buddy </p></Link></div>
+//       </div>
+//       <div className="navbarRight">
+//         <div className="navbar-item"><Link to={routes.SIGN_IN}>SIGN IN</Link></div>
+//       </div>
+//     </div>  
+//   </div>
+// </nav> */}
+
+//       {/* <nav className="navbar navbar-inverse">
+//   <div className="container-fluid">
+//     <div className="navbar-collapse collapse">
+//       <ul className="nav navbar-nav">
+//         <li className="landing"><Link to={routes.LANDING}><img className="logoImg img-responsive" src="assets/images/logo.png" alt=""/><p className="appTitle">Journal Buddy</p></Link></li>
+//         <li className="navbar-item"><Link to={routes.HOME}>Home</Link></li>
+//         <li className="navbar-item"><Link to={routes.ACCOUNT}>Account</Link></li>
+//         <li className="btn-Nav"><SignOutButton /></li>
+//       </ul>
+//     </div>
+//   </div>
+// </nav> */}
+ 
