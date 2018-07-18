7da95f8e78b2013676c16a08ba4fd6885be6f068
Katie Wang <ktwang16@mac.com>
2018-07-18 03:05:09 -0400

    Landing Page updates on alignment

--- src/components/Landing/index.js
+++ src/components/Landing/index.js
@@ -3,33 +3,66 @@ import { Row, Container } from "../Grid";
 import './Landing.css';
 
 const LandingPage = () =>
-  <div>
-    <Container>
+  <div className="landing-page">
+    <Container fluid>
       <Row>
-    <h1>Journal Buddy</h1>
-    <h2>Interactive Journaling</h2>
-    <h3>Lorem ipsum dolor amet cardigan cloud bread williamsburg cornhole 
-      squid chillwave vexillologist tbh jianbing actually mustache street 
-      art ullamco synth chicharrones. Locavore disrupt adaptogen aute 
-      hell of ad. Jean shorts do adaptogen banh mi direct trade gochujang, 
-      succulents poke fanny pack wolf gentrify umami try-hard. Mumblecore 
-      nulla pok pok before they sold out pinterest distillery biodiesel 
-      plaid activated charcoal meh veniam artisan. Yuccie adipisicing 
-      chartreuse, kickstarter poutine cornhole organic.</h3>
+      <div className="col-md-12">
+        <h1>Journal Buddy</h1>
+        <h2>Interactive Journaling</h2>
+        <p>
+          Lorem ipsum dolor amet cardigan cloud bread williamsburg cornhole 
+          squid chillwave vexillologist tbh jianbing actually mustache street 
+          art ullamco synth chicharrones. Locavore disrupt adaptogen aute 
+          hell of ad. Jean shorts do adaptogen banh mi direct trade gochujang, 
+          succulents poke fanny pack wolf gentrify umami try-hard. Mumblecore 
+          nulla pok pok before they sold out pinterest distillery biodiesel 
+          plaid activated charcoal meh veniam artisan. Yuccie adipisicing 
+          chartreuse, kickstarter poutine cornhole organic.
+        </p>
+      </div>
       </Row>
       <Row>
-      <div className="aboutBar">
+      <div className="aboutBar col-md-12">
         <p>
-        Journaling does this and that. Lorem ipsum dolor amet cardigan cloud bread williamsburg cornhole 
-        squid chillwave vexillologist tbh jianbing actually mustache street 
-        art ullamco synth chicharrones. Locavore disrupt adaptogen aute 
-        hell of ad. Jean shorts do adaptogen banh mi direct trade gochujang, 
-        succulents poke fanny pack wolf gentrify umami try-hard. Mumblecore 
-        nulla pok pok before they sold out pinterest distillery biodiesel 
-        plaid activated charcoal meh veniam artisan. Yuccie adipisicing 
-        chartreuse, kickstarter poutine cornhole organic.
+          Journaling does this and that. Lorem ipsum dolor amet cardigan cloud bread williamsburg cornhole 
+          squid chillwave vexillologist tbh jianbing actually mustache street 
+          art ullamco synth chicharrones. Locavore disrupt adaptogen aute 
+          hell of ad. Jean shorts do adaptogen banh mi direct trade gochujang, 
+          succulents poke fanny pack wolf gentrify umami try-hard. Mumblecore 
+          nulla pok pok before they sold out pinterest distillery biodiesel 
+          plaid activated charcoal meh veniam artisan. Yuccie adipisicing 
+          chartreuse, kickstarter poutine cornhole organic.
         </p>
     </div>
+    </Row>
+    <Row>
+      <div className="howTo col-sm-12 ">
+        <h3>How does Journal Buddy work?</h3>
+        <div className="col-sm-3">
+        <h4>FIRST</h4>
+          <p>
+            Journaling does this and that. 
+          </p>
+        </div>
+        <div className="col-sm-3">
+        <h4>SECOND</h4>
+          <p>
+            Journaling does this and that. 
+          </p>
+        </div>
+        <div className="col-sm-3">
+        <h4>THIRD</h4>
+          <p>
+            Journaling does this and that. 
+          </p>
+        </div>
+        <div className="col-sm-3">
+        <h4>FOURTH</h4>
+          <p>
+            Journaling does this and that. 
+          </p>
+        </div>
+      </div>
     </Row>
     </Container>
   </div>
