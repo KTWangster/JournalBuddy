442820199858b00e17ce2d332f470ed414246cca
Katie Wang <ktwang16@mac.com>
2018-07-18 15:08:54 -0400

    the calm before the database storm

--- src/components/Landing/index.js
+++ src/components/Landing/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Row, Container } from "../Grid";
+import * as routes from '../../constants/routes';
+import { Link } from 'react-router-dom';
 import './Landing.css';
 
 const LandingPage = () =>
@@ -23,6 +25,8 @@ const LandingPage = () =>
       </Row>
       <Row>
       <div className="aboutBar col-md-12">
+      <h3 className="about">Why 5-Minutes?</h3>
+      <br/>
         <p>
           Journaling does this and that. Lorem ipsum dolor amet cardigan cloud bread williamsburg cornhole 
           squid chillwave vexillologist tbh jianbing actually mustache street 
@@ -36,36 +40,51 @@ const LandingPage = () =>
     </div>
     </Row>
     <Row>
-      <div className="howTo col-sm-12 ">
-        <h3>How does Journal Buddy work?</h3>
+      <div className="col-sm-12 ">
+        <br/>
+        <h3 className="howWork">How does Journal Buddy work?</h3>
+        <br/><br/><br/>
         <div className="col-sm-3">
-        <span class="glyphicon glyphicon-flash"></span><br/>
+        <span class="glyphicon glyphicon-flash"></span><br/><br/>
         <h4>REFLECT</h4>
+        <br/>
           <p>
             Introspect. Think. Consider. Brainstorm.
           </p>
         </div>
         <div className="col-sm-3">
-        <span class="glyphicon glyphicon-pencil"></span><br/>
+        <span class="glyphicon glyphicon-pencil"></span><br/><br/>
         <h4>WRITE</h4>
+        <br/>
           <p>
             Jot down anything that comes to mind during 5 minutes. 
           </p>
         </div>
         <div className="col-sm-3">
-        <span class="glyphicon glyphicon-refresh"></span><br/>
+        <span class="glyphicon glyphicon-refresh"></span><br/><br/>
         <h4>REPEAT</h4>
+        <br/>
           <p>
             Repeat the process the next day...and the next...and the next...
           </p>
         </div>
         <div className="col-sm-3">
-        <span class="glyphicon glyphicon-star-empty"></span><br/>
+        <span class="glyphicon glyphicon-star-empty"></span><br/><br/>
         <h4>REWARD</h4>
+        <br/>
           <p>
           Reap the benefits of journaling and live a long and healthy life! 
           </p>
         </div>
+      </div>
+      <div className="col-md-12">
+      <button
+    type="button"
+    // onClick={}
+  >
+  <Link to={routes.LANDING}>JOURNAL BUDDY</Link>
+    Get Started!
+  </button>
       </div>
     </Row>
     </Container>
